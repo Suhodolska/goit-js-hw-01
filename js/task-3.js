f74957f5978651fc3de93b12b6e29ce6402dfd30
Innesa Suhodolska <innesasuhodolska@gmail.com>
2025-03-11 21:20:56 +0200

    updatehm1

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,14 @@
 function getElementWidth(content, padding, border) {
-    return getElementWidth.offsetWidth;
+    const contentWidth = parseFloat(content);
+    const paddingtWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border)  * 2;
+    return contentWidth + paddingtWidth + borderWidth;
 }
 
 getElementWidth("50px", "8px", "4px")
 getElementWidth("60px", "12px", "8.5px")
 getElementWidth("200px", "0px", "0px")
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px")); 
+console.log(getElementWidth("200px", "0px", "0px")); 
