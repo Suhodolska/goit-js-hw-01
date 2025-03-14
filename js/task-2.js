22f51184d0f36a293c5e06783f86f51624bb6314
Innesa Suhodolska <innesasuhodolska@gmail.com>
2025-03-14 20:56:13 +0200

    donehm1

--- js/task-2.js
+++ js/task-2.js
@@ -2,10 +2,6 @@ function getShippingMessage(country, price, deliveryFee) {
     return (`Shipping to ${country} will cost ${price + deliveryFee} credits`)
 }
 
-getShippingMessage("Australia", 120, 50)
-getShippingMessage("Germany", 80, 20)
-getShippingMessage("Sweden", 100, 20)
-
 
 console.log(getShippingMessage("Australia", 120, 50)); 
 console.log(getShippingMessage("Germany", 80, 20)); 
--- js/task-3.js
+++ js/task-3.js
@@ -5,9 +5,6 @@ function getElementWidth(content, padding, border) {
     return contentWidth + paddingtWidth + borderWidth;
 }
 
-getElementWidth("50px", "8px", "4px")
-getElementWidth("60px", "12px", "8.5px")
-getElementWidth("200px", "0px", "0px")
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px")); 
